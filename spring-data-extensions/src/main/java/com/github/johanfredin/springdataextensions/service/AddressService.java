ea19c6b33d9ad972eb7d4cc54dd5d1d27198a6ec
Johan Fredin <johanfredin@fastmail.com>
2018-12-30 21:09:04 +0100

    Created new demo project that uses the api, removed all example code from the api to that project

--- spring-data-extensions/src/main/java/com/github/johanfredin/springdataextensions/service/AddressService.java
+++
@@ -1,7 +0,0 @@
-package com.github.johanfredin.springdataextensions.service;
-
-import com.github.johanfredin.springdataextensions.domain.Address;
-import com.github.johanfredin.springdataextensions.repository.AddressRepository;
-
-public interface AddressService extends ServiceBase<Long, Address, AddressRepository> {
-}
--- spring-data-extensions/src/main/java/com/github/johanfredin/springdataextensions/util/comparator/DateComparator.java
+++ spring-data-extensions/src/main/java/com/github/johanfredin/springdataextensions/util/comparator/DateComparator.java
@@ -7,5 +7,26 @@ import java.util.Comparator;
 public interface DateComparator extends Comparator<ChangeDateHolder> {
 
     @Override
-    int compare(ChangeDateHolder o1, ChangeDateHolder o2);
+    default int compare(ChangeDateHolder o1, ChangeDateHolder o2) {
+        try {
+
+            String d1 = o1.getLastChangeDate();
+            String d2 = o2.getLastChangeDate();
+
+            long date1 = Long.parseLong(d1.replaceAll(":", "").replaceAll("-", "").replaceAll(" ", ""));
+            long date2 = Long.parseLong(d2.replaceAll(":", "").replaceAll("-", "").replaceAll(" ", ""));
+
+            if (date1 < date2) {
+                return 1;
+            } else if (date1 == date2) {
+                return 0;
+            }
+
+            return -1;
+
+        } catch (NumberFormatException | NullPointerException ex) {
+            // 1 in this case would mean ascending order
+            return 1;
+        }
+    }
 }
